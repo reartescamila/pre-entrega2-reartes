fe7c8a1a75ee36fb34106e1059b2e398f099b04d
Camila Reartes <reartescami@gmail.com>
2023-02-15 22:17:42 -0300

    reentrega

--- index.js
+++ index.js
@@ -2,161 +2,146 @@
 PRE ENTREGA 2 REARTES CAMILA 
 */
 
+// ----
+// CREADOR DE STOCK
+// ----
+let colectivos = [];
+
+function agregarColectivo() {
+  let cantidad = parseInt(
+    prompt("Ingrese la cantidad de pasajes que va agregar a su stock")
+  );
+  let origen = prompt("Ingrese la terminal de origen").toLowerCase();
+  let destino = prompt("Ingrese la terminal de destino").toLowerCase();
+  let precio = parseInt(prompt("Ingrese el precio del producto"));
+  if (
+    colectivos.some((item) => item.origen === origen) &&
+    colectivos.some((item) => item.destino === destino)
+  ) {
+    alert("Ya existe este colectivo");
+  } else {
+    alert("Su Colectivo ha sido agregado correctamente");
+    let objeto = {
+      id: colectivos.length + 1,
+      cantidad,
+      origen,
+      destino,
+      precio,
+    };
+    colectivos.push(objeto);
+    console.log(colectivos);
+  }
+}
+
+// COMPRA DE PASAJES
+
 let pasajeros = [];
 
 const suma = (a, b) => a + b;
 const resta = (a, b) => a - (a * b) / 100;
 const iva = (x) => x * 1.21;
 
-function agregarPasaje() {
-  let origen = prompt(`
-  Ingrese ciudad de origen. OPCIONES:
-  -CORDOBA
-  -SAN LUIS
-  -BUENOS AIRES`).toLowerCase();
-  let destino = prompt(`
-  Ingrese ciudad de destino. OPCIONES:
-  -CORDOBA
-  -SAN LUIS
-  -BUENOS AIRES`).toLowerCase();
+function comprarPasaje() {
+  let origen = prompt(`Ingrese terminal de origen`).toLowerCase();
+  let destino = prompt(`Ingrese terminal de destino`).toLowerCase();
   let dni = parseInt(prompt("Ingrese el DNI del pasajero"));
   let nombrePasajero = prompt("Ingrese NOMBRE Y APELLIDO del pasajero");
 
-  if (origen === "cordoba" && destino === "buenos aires") {
-    alert("El pasaje ha sido comprado con exito");
+  if (
+    colectivos.some((item) => item.origen === origen) &&
+    colectivos.some((item) => item.destino === destino)
+  ) {
     let objeto = {
       id: pasajeros.length + 1,
+      cantidad: +1,
       origen,
       destino,
+      precio,
       dni,
       nombrePasajero,
-      precio: 1500,
-      descuento: 5,
-      precioTotal: iva(resta(1500, 5)),
     };
     pasajeros.push(objeto);
     console.log(pasajeros);
-  } else if (origen === "cordoba" && destino === "san luis") {
     alert("El pasaje ha sido comprado con exito");
-    let objeto = {
-      id: pasajeros.length + 1,
-      origen,
-      destino,
-      dni,
-      nombrePasajero,
-      precio: 1200,
-      descuento: 15,
-      precioTotal: iva(resta(1200, 15)),
-    };
-    pasajeros.push(objeto);
-    console.log(pasajeros);
-  } else if (origen === "san luis" && destino === "cordoba") {
-    alert("El pasaje ha sido comprado con exito");
-    let objeto = {
-      id: pasajeros.length + 1,
-      origen,
-      destino,
-      dni,
-      nombrePasajero,
-      precio: 1000,
-      descuento: 10,
-      precioTotal: iva(resta(1000, 10)),
-    };
-    pasajeros.push(objeto);
-    console.log(pasajeros);
-  } else if (origen === "san luis" && destino === "buenos aires") {
-    alert("El pasaje ha sido comprado con exito");
-    let objeto = {
-      id: pasajeros.length + 1,
-      origen,
-      destino,
-      dni,
-      nombrePasajero,
-      precio: 2000,
-      descuento: 15,
-      precioTotal: iva(resta(2000, 15)),
-    };
-    pasajeros.push(objeto);
-    console.log(pasajeros);
-  } else if (origen === "buenos aires" && destino === "cordoba") {
-    alert("El pasaje ha sido comprado con exito");
-    let objeto = {
-      id: pasajeros.length + 1,
-      origen,
-      destino,
-      dni,
-      nombrePasajero,
-      precio: 1500,
-      descuento: 15,
-      precioTotal: iva(resta(1500, 15)),
-    };
-    pasajeros.push(objeto);
-    console.log(pasajeros);
-  } else if (origen === "buenos aires" && destino === "san luis") {
-    alert("El pasaje ha sido comprado con exito");
-    let objeto = {
-      id: pasajeros.length + 1,
-      origen,
-      destino,
-      dni,
-      nombrePasajero,
-      precio: 1000,
-      descuento: 10,
-      precioTotal: iva(resta(1000, 10)),
-    };
-    pasajeros.push(objeto);
-    console.log(pasajeros);
-  } else {
-    alert("USTED NO HA SELECCIONADO SU PASAJE CORRECTAMENTE");
+  } else if (
+    colectivos.some((item) => item.origen != objeto) &&
+    colectivos.some((item) => item.destino != objeto)
+  ) {
+    alert("USTED NO HA INGRESADO LAS TERMINALES CORRECTAMENTE");
   }
 }
 
 function buscar() {
-  let dni = parseInt(prompt("Ingrese el DNI del pasajero que desea consultar"));
-  let busqueda = pasajeros.filter((item) => item.dni === dni);
+  let origen = prompt(
+    "Ingrese el NOMBRE DE ORIGEN para ver los pasajes disponibles"
+  ).toLowerCase();
+  let searchFilter = colectivos.filter((item) => item.origen === origen);
   let mensaje = "";
-  busqueda.forEach((item) => {
+
+  searchFilter.forEach((item) => {
     mensaje += `
     ID: ${item.id}
     TERMINAL DE ORIGEN: ${item.origen}
     TERMINAL DE DESTINO: ${item.destino}
-    DNI: ${item.dni}
-    NOMBRE Y APELLIDO: ${item.nombrePasajero}
-    Precio: $${item.precio}
-    Descuento: %${item.descuento}
-    PRECIO TOTAL (+iva): $${item.precioTotal}
+    Precio: $${item.precio}}
   `;
   });
 
   alert(mensaje);
 }
 
-function ordenarDeMayor() {
-  console.log(
-    pasajeros.sort((a, b) => {
-      if (a.precio > b.precioTotal) {
-        return -1;
-      }
-      if (a.precio < b.precioTotal) {
-        return 1;
-      }
-      return 0;
-    })
-  );
+alert("INICIO DEL PROCESO DE AGREGADO DE STOCK");
+
+let decisionStock = prompt(
+  "desea agregar o seguir agregando colectivos? si o no"
+).toLowerCase();
+while (decisionStock != "no" && decisionStock != "si") {
+  alert("por favor ingresa si o no");
+  decisionStock = prompt(
+    "desea agregar o seguir agregando colectivos? si o no"
+  ).toLowerCase();
 }
 
-agregarPasaje();
-buscar();
-ordenarDeMayor(); //Observable en la consola
+if (decisionStock == "si") {
+  while (decisionStock != "no") {
+    agregarColectivo();
+    decisionStock = prompt(
+      "desea agregar o seguir agregando colectivos? si o no"
+    ).toLowerCase();  
+  }
+} else if (decisionStock == "no") {
+  alert("HA FINALIZADO EL PROCESO DE AGREGADO DE STOCK");
+}
 
-while (
-  (seguirAgregando =
-    prompt(
-      `Si desea seguir COMPRANDO Pasajes, presione ENTER.
-   Si desea finalizar escriba: NO`
-    ).toLowerCase() != "no")
-) {
-  agregarPasaje();
+// while (decisionStock != "no") {
+//   decisionStock = prompt(
+//     "desea agregar o seguir agregando colectivos? si o no"
+//   ).toLowerCase();  
+//   agregarColectivo();
+// }
+
+// BUSQUEDA POR FILTRADO
+let filter = prompt(`
+Si desea filtrar algun producto escriba SI.
+De lo contrario escriba NO`).toLowerCase();
+
+while (filter != "no" && filter != "si") {
+  alert("Ingresa si o no");
+  filter = prompt(`
+  ingrese la terminal de origen que desea filtrar`).toLowerCase();
+}
+
+if (filter == "si") {
+  let colectivosAll = colectivos.map(
+    (item) => `
+  NUESTROS VIAJES:
+  origen: ${item.origen}
+  destino: ${item.destino}
+  Preecio: $${item.precio}
+    `
+  );
+  alert(colectivosAll);
   buscar();
-  ordenarDeMayor(); //Observable en la consola
+} else if (filter == "no") {
+  alert("LA BUSQUEDA TERMINADO");
 }
